647672e850b7a947decd3d4aa85dbe55aed26359
Quotennial <yusufsohoye@googlemail.com>
2020-04-01 18:00:59 +0100

    added env success

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 # Understanding JAMs
 
 # Codebase
+
+### Using Virtual Enviroments
+
+A good [primer](https://realpython.com/python-virtual-environments-a-primer/) on the uses of venv.
+
+```bash
+python3 -m venv venv # to create new venv in directory
+source venv/bin/activate # to activate it
+pip install -r requirements.txt
+deactivate # to deactivate
+```
+
+### Using venv in Jupyter notebook
+
+```bash
+$ python -m venv projectname
+$ source projectname/bin/activate
+(venv) $ pip install ipykernel
+(venv) $ ipython kernel install --user --name=projectname
+```
+
+#
+Then install requirements 
 pip install -r requirments.txt
--- config.py
+++ config.py
@@ -1,14 +1,14 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 class Essex_Shape_Config():
     root_folder = "geog_files"
     OUPUT_GEOJSON = f"{root_folder}/essex_output_area.geojson"
     #Obtained from https://geoportal.statistics.gov.uk/datasets/output-area-to-county-and-unitary-authority-december-2018-lookup-in-england
     MAPPING_TABLE = f"{root_folder}/Output_Area_to_County_and_Unitary_Authority_December_2018_Lookup_in_England.csv" 
-
     LAU_SHP_FILE = f"{root_folder}/Local_Authority_Districts_April_2019_Boundaries_UK_BFC"
-
-
     LAD_boundaries = "https://opendata.arcgis.com/datasets/8edafbe3276d4b56aec60991cbddda50_2.geojson"
-
     COUNT_MAP_LAD = f"{root_folder}/Local_Authority_District_to_County_December_2015_Lookup_in_England.csv" 
 
 class Web_Scrape_Config():
@@ -22,15 +22,17 @@ class Web_Scrape_Config():
 
 class Zoopla_Config():
     base_url = "http://api.zoopla.co.uk/api/v1/property_listings.json"
-    key = "u5sevpae3gdup7ft7jjvkppp"
-    # key = "wft89y7dtrr9q9byrsjs32cz"
+    key = os.getenv("zoopla_key_one") 
+    # key = os.getenv("zoopla_key_two")
+    #  
     area = "Essex"
     summarised= "True"
     include_rented = 1
     include_sold = 1
     page_size = 100
     page_num = 1
-    params = {"api_key": key, "area": area, "summarised":summarised,  "include_rented": include_rented, 
-    "include_sold": include_sold, "page_size":page_size, "page_num":page_num, "include_rented": include_rented}
-
+    params = {"api_key": key, "area": area, "summarised":summarised,
+        "include_rented": include_rented,"include_sold": include_sold, 
+        "page_size":page_size, "page_num":page_num}
+    
     output_add = "web_data/zoopla_dump.csv"
